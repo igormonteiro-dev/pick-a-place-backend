af930b91100616f296c8ee09103481cefc98f142
Igor Dev <igormonteiro.dev@gmail.com>
2022-09-16 16:51:58 +0200

    done

--- routes/RandomPlaces.routes.js
+++ routes/RandomPlaces.routes.js
@@ -3,7 +3,7 @@ const Places = require("../models/Place.model");
 
 router.get("/random", async (req, res, next) => {
   try {
-    const randomPlaces = await Places.agggregate([{ $sample: { size: 4 } }]);
+    const randomPlaces = await Places.aggregate([{ $sample: { size: 4 } }]);
     return res.status(200).json(randomPlaces);
   } catch (error) {
     next(error);
